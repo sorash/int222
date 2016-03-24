61e4490d1c20f27d5a75549372f23fee3453ebac
sorash <so.as67@yahoo.com>
2016-03-23 20:14:41 -0400

    beginning of assignment 3

---
+++ assignments/assign3/javascript/mortgage.js
@@ -0,0 +1,102 @@
+    //********************************************************************************//
+    //* Name :                                                                       *//
+    //* zenit login : int222_161                                                     *//
+    //********************************************************************************//
+    //********************************************************************************//
+    //*   Do not modify any statements in detailPaymentCalculation function          *//
+    //********************************************************************************//
+
+function detailPaymentCalculation(mortAmount,mortDownPayment,mortRate,mortAmortization) {
+
+    //********************************************************************************//
+    //*   This function calculates the monthly payment based on the following:       *//
+    //*                                                                              *//
+    //*               M = P [ i(1 + i)n ] / [ (1 +  i)n - 1]                         *//
+    //*                                                                              *//
+    //*   Note: This function also updates the payment amount on the form            *//
+    //********************************************************************************//
+     var paymentError = "";
+     var v = mortAmount * 1;
+     var d = mortDownPayment * 1;
+     var i = mortRate * 1;
+     var y = mortAmortization * 1;
+     var a = v - d;
+         i = i/100/12;
+         n = y * 12;
+     var f = Math.pow((1+i),n);
+
+     var p = (a * ((i*f)/(f-1))).toFixed(2);
+
+     if (p=="NaN" || p=="Infinity") {
+         document.forms[0].payment.value = "";
+     }
+     else {
+           document.forms[0].payment.value = p;
+     }
+
+} // End of detailPaymentCalculation function
+
+
+function calculatePayment() {   
+
+    //********************************************************************************//
+    //*   You will need to call the functions that validate the following:           *//
+    //********************************************************************************//
+    //*        (1)              (2)              (3)             (4)                 *//
+    //********************************************************************************//
+    //*   Property value  -  Down payment  -  Interest rate -  Amortization          *//
+    //********************************************************************************//
+    //*   If there are no errors, then call                                          *//
+    //*                                                                              *//
+    //*      detailPaymentCalculation(...., ......, ......, ......);                 *//
+    //*                                                                              *//
+    //*   and make sure to pass the four values in the order shown above.            *//
+    //*                                                                              *//
+    //********************************************************************************//
+    //*   If there are errors, present the client the following message in the       *//
+    //*   reserved area on the form:                                                 *//
+    //*                                                                              *//
+    //*   Please complete the form first and then click on Calculate Monthly Payment *//
+    //*                                                                              *//
+    //********************************************************************************//
+
+} // End of calculatePayment function
+
+
+
+function formValidation() {
+
+    //***************************************************************************************//
+    //*                                                                                     *//
+    //* This function calls the different functions to validate all required fields         *//
+    //*                                                                                     *//
+    //* Once you have called and validated all field, determine if any error(s)             *//
+    //*  have been encountered                                                              *//
+    //*                                                                                     *//
+    //* If any of the required fields are in error:                                         *//
+    //*                                                                                     *//
+    //*    present the client with a list of all the errors in reserved area                *//
+    //*         on the form and                                                             *//
+    //*          don't submit the form to the CGI program in order to allow the             *//
+    //*          client to correct the fields in error                                      *//
+    //*                                                                                     *//
+    //*    Error messages should be meaningful and reflect the exact error condition.       *//
+    //*                                                                                     *//
+    //*    Make sure to return false                                                        *//
+    //*                                                                                     *//
+    //* Otherwise (if there are no errors)                                                  *//
+    //*                                                                                     *//
+    //*    Recalculate the monthly payment by calling                                       *//
+    //*      detailPaymentCalculation(mortAmount,mortDownPayment,mortRate,mortAmortization) *//
+    //*                                                                                     *//
+    //*    Change the 1st. character in the field called client to upper case               *//
+    //*                                                                                     *//
+    //*    Change the initial value in the field called jsActive from N to Y                *//
+    //*                                                                                     *//
+    //*    Make sure to return true in order for the form to be submitted to the CGI        *//
+    //*                                                                                     *//
+    //***************************************************************************************//
+
+
+
+} // End of completeFormValidation
